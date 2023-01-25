93c82263c1e82f2e2f14ebd9023147725870c462
Andrew Magid <magid.andrew@gmail.com>
2023-01-24 22:28:12 -0500

    added homepage and gh-pages support

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   define: {
     'process.env': process.env
   },
@@ -25,5 +26,5 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true,
     }
-  } 
+  }
 })
